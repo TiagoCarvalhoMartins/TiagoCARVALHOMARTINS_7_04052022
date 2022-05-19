1aa75ffbcf5177160e969b127e093f26545a731b
Tiago CARVALHO MARTINS <carvalhomartins.tiago@gmail.com>
2022-05-19 10:44:52 +0200

    recipes.json creation

--- scripts/factories/recipeCard.js
+++ scripts/factories/recipeCard.js
@@ -1,7 +1,10 @@
 function recipeFactory(data) {
-    const { name, time, description, ingredient, quantity, unit  } = data;
+    const { name, time, description, ingredients, quantity, unit  } = data;
 
     function getRecipeCardDOM() {
+        let ingredientsTemplate = "";
+        //ingredientsTemplate += `<strong>${ingredients[0].ingredient}:</strong> ${ingredients[0].quantity}${ingredients[0].unit}<br>`
+
         let cardTemplate = 
         `<div class="card col-lg-4 col-md-6 col-sm-12">
             <div class="thumbnailContainer">
@@ -9,10 +12,10 @@ function recipeFactory(data) {
             </div>
             <div class="texte card-body">
                 <div class="titleTime d-flex flex-row justify-content-between">
-                    <h2>${recipes.name}</h2>
+                    <h2>${name}</h2>
                     <div class="time d-flex flex-row">
                         <span class="fa-regular fa-clock"></span>
-                        <h3>${recipes.time} min</h3>
+                        <h3>${time} min</h3>
                     </div>
                 </div>
                 <div class="recipe">
@@ -20,22 +23,19 @@ function recipeFactory(data) {
                         <div class="row d-flex flex-row">
                             <div class="neededIngredientsList col-6">
                                 <h4>
-                                    <strong>${recipes.ingredients.ingredient[0]}:</strong> ${recipes.ingredients.quantity[0]}${recipes.ingredients.unit[0]}<br>
-                                    <strong>${recipes.ingredients.ingredient[1]}:</strong> ${recipes.ingredients.quantity[1]}${recipes.ingredients.unit[1]}<br>
-                                    <strong>${recipes.ingredients.ingredient[2]}:</strong> ${recipes.ingredients.quantity[2]}${recipes.ingredients.unit[2]}<br>
-                                    <strong>${recipes.ingredients.ingredient[3]}:</strong> ${recipes.ingredients.quantity[3]}${recipes.ingredients.unit[3]}<br>
-                                    <strong>${recipes.ingredients.ingredient[4]}:</strong> ${recipes.ingredients.quantity[4]}${recipes.ingredients.unit[4]}<br>
+                                ${ingredientsTemplate}
                                 </h4>
                             </div>
                             <div class="instructions col-6">
-                                <p>${recipes.description}</p>
+                                <p>${description}</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>`;
-        cardRecipes.innerHTML = cardTemplate
+        //cardRecipes.innerHTML = cardTemplate
+        return (cardTemplate)
     }
-    return { name, time, description, ingredient, quantity, unit, getRecipeCardDOM }
+    return { name, time, description, ingredients, quantity, unit, getRecipeCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,6 @@
 async function getRecipes() {
     // Penser à remplacer par les données récupérées dans le json
-    let response = await fetch("data/recipes.js")
+    let response = await fetch("data/recipes.json")
     let myJSON = await response.json();
        
     return (myJSON)
@@ -8,27 +8,28 @@ async function getRecipes() {
 
 function displayRecipes(recipes) {
     const cardRecipes = document.querySelector(".cardRecipes");
-
+    cardRecipes.innerHTML = ""
     recipes.forEach((recipe) => {
         const recipeModel = recipeFactory(recipe);
         const recipeCardDOM = recipeModel.getRecipeCardDOM();
-        cardRecipes.appendChild(recipeCardDOM);
+        cardRecipes.innerHTML += recipeCardDOM;
     });
 };
 
 
 async function init() {
-    const { recipes } = await getRecipes();
+    const recipes  = await getRecipes();
     displayRecipes(recipes);
 
     //filter
     let selectedIngredient = "Concombre";
 
-    function filterIngredient (recipes) {
-        if (recipes.ingredients.ingredient === selectedIngredient) {
+   function filterIngredient(selectedIngredient) {
+       if (recipes.ingredients.ingredient === selectedIngredient) {
             return displayRecipes(recipesFiltered)
         }
     }
+    filterIngredient(selectedIngredient)
 };
 
 init();
